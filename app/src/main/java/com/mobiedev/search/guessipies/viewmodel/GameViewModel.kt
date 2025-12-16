251ea9ff6ceb8c82ce15d2d47363879b0264cf77
ross-bell-bbc <ross.bell@bbc.co.uk>
2025-12-16 15:39:58 +0000

    Added BE model for guessipes

--- app/src/main/java/com/mobiedev/search/guessipies/viewmodel/GameViewModel.kt
+++ app/src/main/java/com/mobiedev/search/guessipies/viewmodel/GameViewModel.kt
@@ -1,5 +1,6 @@
 package com.mobiedev.search.guessipies.viewmodel
 
+import android.system.Os.link
 import androidx.lifecycle.ViewModel
 import com.mobiedev.search.guessipies.models.Chain
 import com.mobiedev.search.guessipies.models.Link
@@ -30,73 +31,35 @@ class GameViewModel : ViewModel() {
 
     fun onClickGuess(recipeGuessed: Recipe) {
         uiState.value.currentRecipe.linksToOtherRecipe(otherRecipe = recipeGuessed)?.let { link ->
-            val newLinks = buildList {
-                addAll(uiState.value.chain.links)
-                add(link)
-            }
-            _uiState.update {
-                uiState.value.copy(
-                    currentRecipe = recipeGuessed,
-                    chain = uiState.value.chain.copy(
-                        links = newLinks,
-                        score = newLinks.size
-                    )
-                )
-            }
+            updateLinks(
+                recipeGuessed = recipeGuessed,
+                link = link
+            )
+            // fetch new items
         } ?: run {
-            _uiState.update {
-                uiState.value.copy(gameLive = false)
-            }
+            endGame()
+        }
+    }
+
+    private fun updateLinks(recipeGuessed: Recipe, link: Link) {
+        val newLinks = buildList {
+            addAll(uiState.value.chain.links)
+            add(link)
+        }
+        _uiState.update {
+            uiState.value.copy(
+                currentRecipe = recipeGuessed, // TODO: remove
+                chain = uiState.value.chain.copy(
+                    links = newLinks,
+                    score = newLinks.size
+                )
+            )
+        }
+    }
+
+    private fun endGame() {
+        _uiState.update {
+            uiState.value.copy(gameLive = false)
         }
     }
 }
-
-private val stubChain = Chain(
-    links = listOf(
-        Link(
-            recipe1 = Recipe(
-                title = "Recipe 1",
-                ingredients = listOf("R1 & R2 matcher", "xyz")
-            ),
-            ingredient = "R1 & R2 matcher",
-            recipe2 = Recipe(
-                title = "Recipe 2",
-                ingredients = listOf("R1 & R2 matcher", "R2 & R3 matcher", "abc")
-            )
-        ),
-        Link(
-            recipe1 = Recipe(
-                title = "Recipe 2",
-                ingredients = listOf("R1 & R2 matcher", "R2 & R3 matcher", "123")
-            ),
-            ingredient = "R2 & R3 matcher",
-            recipe2 = Recipe(
-                title = "Recipe 3",
-                ingredients = listOf("R2 & R3 matcher", "R3 & R4 matcher")
-            )
-        ),
-        Link(
-            recipe1 = Recipe(
-                title = "Recipe 3",
-                ingredients = listOf("R2 & R3 matcher", "R3 & R4 matcher")
-            ),
-            ingredient = "R3 & R4 matcher",
-            recipe2 = Recipe(
-                title = "Recipe 4",
-                ingredients = listOf("R3 & R4 matcher", "R4 & R5 matcher")
-            )
-        ),
-        Link(
-            recipe1 = Recipe(
-                title = "Recipe 4",
-                ingredients = listOf("R4 & R5 matcher", "cheese")
-            ),
-            ingredient = "R4 & R5 matcher",
-            recipe2 = Recipe(
-                title = "Recipe 5",
-                ingredients = listOf("R4 & R5 matcher", "beans")
-            )
-        )
-    ),
-    score = 0
-)
