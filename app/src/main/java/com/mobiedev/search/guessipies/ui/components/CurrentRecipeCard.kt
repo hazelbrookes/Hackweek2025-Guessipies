9445979f9ba0c2cae58991e201dcd963ed4abf89
ross-bell-bbc <ross.bell@bbc.co.uk>
2025-12-17 14:49:12 +0000

    loading images

--- app/src/main/java/com/mobiedev/search/guessipies/ui/components/CurrentRecipeCard.kt
+++ app/src/main/java/com/mobiedev/search/guessipies/ui/components/CurrentRecipeCard.kt
@@ -1,6 +1,10 @@
 package com.mobiedev.search.guessipies.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -11,13 +15,22 @@ import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImagePainter
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 import com.mobiedev.search.guessipies.viewmodel.GameUiState
 
 @Composable
@@ -30,6 +43,7 @@ fun CurrentRecipeCard(uiState: GameUiState) {
             .fillMaxWidth()
             .padding(20.dp)
     ) {
+        RecipeImage(uiState.currentRecipe.imageUrl ?: "")
         Text(
             text = uiState.currentRecipe.title,
             style = MaterialTheme.typography.titleLarge,
@@ -52,6 +66,41 @@ fun CurrentRecipeCard(uiState: GameUiState) {
                     text = ingredient,
                     textAlign = TextAlign.Center,
                     modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RecipeImage(url: String) {
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(100.dp)
+    ) {
+        val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(data = url)
+                .crossfade(true)
+                .build()
+        )
+        val state by painter.state.collectAsState()
+
+        when (state) {
+            is AsyncImagePainter.State.Success -> {
+                Image(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    painter = painter,
+                    contentDescription = ""
+                )
+            }
+
+            else -> {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Red)
+                        .fillMaxWidth()
                 )
             }
         }
